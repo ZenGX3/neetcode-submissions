fbb080c7362940395649fc6c10de715796e9288c
Sohamdeep Mandal <96684387+ZenGX3@users.noreply.github.com>
2026-08-04 23:24:37 +0530

    Add: trapping-rain-water - submission-5

---
+++ Data Structures & Algorithms/trapping-rain-water/submission-5.py
@@ -0,0 +1,14 @@
+class Solution:
+    def trap(self, h: List[int]) -> int:
+        ml = []
+        mr = []
+        c, d = 0, 0
+        for x in h:
+            c = max(c, x)
+            ml.append(c)
+        for x in h[::-1]:
+            d = max(d, x)
+            mr.append(d)
+        mr.reverse()
+        minlr = [min(ml[i], mr[i]) for i in range(len(h))]
+        return sum([max(minlr[i]-h[i], 0) for i in range(len(h))])
